90509c80cb7e63e99d74ce4150b8cf9ddffd5a61
tnhnam <hoangnamhr050117@gmail.com>
2024-04-15 15:55:07 +0700

    add transfer fn

--- libs/BaseInterfaces/SpotApiInterface.ts
+++ libs/BaseInterfaces/SpotApiInterface.ts
@@ -1,11 +1,12 @@
+import { TransferRequest } from "exchanges/binance/RequestParams";
 import {
   type CancelOrderPayload,
   type GetDepositAddressPayload,
   type GetKlinesRequestPayload,
   type GetOrderPayload,
   type PlaceSpotOrderPayload,
-  type WithdrawRequestPayload
-} from '../RequestConfig';
+  type WithdrawRequestPayload,
+} from "../RequestConfig";
 import {
   type FormattedKline,
   type FormattedOrder,
@@ -14,28 +15,29 @@ import {
   type FormattedTicker,
   type FormattedWithdrawalTransaction,
   type OrderResponse,
-  type FormattedDepositTransaction
-} from '../ResponseConfig';
+  type FormattedDepositTransaction,
+} from "../ResponseConfig";
 interface SpotApiInterface {
-  getKlines: (payload: GetKlinesRequestPayload) => Promise<FormattedKline[]>
-  getTicker: (symbol?: string) => Promise<FormattedTicker | any>
-  getBalances: () => Promise<FormattedBalance[]>
-  getOrderDetail: (payload: GetOrderPayload) => Promise<FormattedOrder>
+  getKlines: (payload: GetKlinesRequestPayload) => Promise<FormattedKline[]>;
+  getTicker: (symbol?: string) => Promise<FormattedTicker | any>;
+  getBalances: () => Promise<FormattedBalance[]>;
+  getOrderDetail: (payload: GetOrderPayload) => Promise<FormattedOrder>;
   getDepositAddress: (
     payload: GetDepositAddressPayload
-  ) => Promise<FormattedDepositAddress>
+  ) => Promise<FormattedDepositAddress>;
   getDepositHistories: (
     coin: string,
     startTime?: number
-  ) => Promise<FormattedDepositTransaction[]>
-  getWithdrawal: (id: string) => Promise<FormattedWithdrawalTransaction>
+  ) => Promise<FormattedDepositTransaction[]>;
+  getWithdrawal: (id: string) => Promise<FormattedWithdrawalTransaction>;
   withdraw: (
     payload: WithdrawRequestPayload
-  ) => Promise<FormattedWithdrawalTransaction>
+  ) => Promise<FormattedWithdrawalTransaction>;
   placeOrder: (
     payload: PlaceSpotOrderPayload
-  ) => Promise<FormattedOrder | OrderResponse>
-  cancelOrder: (payload: CancelOrderPayload) => Promise<FormattedOrder>
+  ) => Promise<FormattedOrder | OrderResponse>;
+  cancelOrder: (payload: CancelOrderPayload) => Promise<FormattedOrder>;
+  transfer?: (payload: TransferRequest) => Promise<any>;
 }
 
 export default SpotApiInterface;
